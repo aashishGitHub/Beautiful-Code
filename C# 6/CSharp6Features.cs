323837dd9e78656be0812d4124c25d09c18de0da
aashishGitHub <aashish123pqr@gmail.com>
2017-06-03 22:22:08 +0530

    Adding new C# 6 features

---
+++ C# 6/CSharp6Features.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CSharp6Features
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
